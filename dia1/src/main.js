a81c12b287c253df555efc56221d577e26ef97c6
NathanOliveirajf <nathanoliveira003jf@gmail.com>
2021-08-17 15:13:07 -0300

    toggle button criado

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -1,6 +1,18 @@
 import "./style.css";
 
-document.querySelector("[data-js='app']").innerHTML = `
+const app = document.querySelector("[data-js='app']");
+const alter = document.querySelector("[data-js='alter']");
+
+app.innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `;
+
+alter.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (app.style.display === "none") {
+    app.style.display = "block";
+  } else {
+    app.style.display = "none";
+  }
+});
