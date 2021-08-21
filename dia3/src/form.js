0357c4b62a2a8afc301ebc5d40309c0bc66f57c9
NathanOliveirajf <nathanoliveira003jf@gmail.com>
2021-08-21 11:57:30 -0300

    ex2 feito

--- dia3/src/form.js
+++ dia3/src/form.js
@@ -1,11 +1,11 @@
 const inputNome = document.querySelector("[data-js='nome']");
+const form = document.querySelector("[data-js='form']");
 
 inputNome.addEventListener("input", (event) => {
   if (event.target.value === "") {
     return;
   }
   const str = event.target.value.toLowerCase().split(" ");
-
   if (str.length > 0) {
     const ar = str.map((i) => {
       if (["de", "da", "do", "dos"].includes(i)) {
@@ -16,3 +16,34 @@ inputNome.addEventListener("input", (event) => {
     console.log(ar.join(" "));
   }
 });
+
+const select = document.createElement("select");
+select.setAttribute("multiple", true);
+
+["#4169E1", "#DC143C", "#006400", "#A0522D"].forEach((color) => {
+  const option = document.createElement("option");
+  option.value = color;
+  option.textContent = color;
+  select.appendChild(option);
+});
+
+form.appendChild(select);
+const containerDiv = document.createElement("div");
+
+select.addEventListener("change", (e) => {
+  containerDiv.innerHTML = "";
+  Array.from(e.target.selectedOptions).forEach((option) => {
+    const div = createElementDiv(option.value);
+    containerDiv.appendChild(div);
+  });
+});
+
+function createElementDiv(color) {
+  const div = document.createElement("div");
+  div.style.display = "block";
+  div.style.width = "100px";
+  div.style.height = "100px";
+  div.style.backgroundColor = color;
+  return div;
+}
+document.body.appendChild(containerDiv);
